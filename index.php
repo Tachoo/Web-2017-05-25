a20c09a99d76aded0ea307af6e346f28d1bd99ae
Sergio Anastacio Angulo Rosales <senao28@icloud.com>
2017-05-25 14:56:37 -0600

    Origin Commit

---
+++ index.php
@@ -0,0 +1,6 @@
+
+<?php
+/*Vamos a Redireccionar la entrada del dominio .xyz  a  una archivo de una capeta en automatico*/
+header("Location: http://www.tachoo.zyz/home/index.html");
+/*Esto es todo lo que va a hacer este php por el momento*/
+?>
