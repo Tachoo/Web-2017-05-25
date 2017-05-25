457b8b74e619937d1dd2099a35cc09b530acdff1
Sergio Anastacio Angulo Rosales <senao28@icloud.com>
2017-05-25 15:05:59 -0600

    The base of the website

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 
 <?php
 /*Vamos a Redireccionar la entrada del dominio .xyz  a  una archivo de una capeta en automatico*/
-header("Location: http://www.tachoo.zyz/home/index.html");
+header("Location: http://www.tachoo.xyz/home/index.html");
 /*Esto es todo lo que va a hacer este php por el momento*/
 ?>
